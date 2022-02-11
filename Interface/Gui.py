abd1923093f8acb2dcee9942ed68cd2a73072b1c
AcilRestu12 <arieskarestu214@gmail.com>
2022-02-11 11:17:23 +0700

    Edit GUI, add menu position order and checkbutton for erase symbol

--- Interface/Gui.py
+++ Interface/Gui.py
@@ -1,5 +1,6 @@
 from cgitb import text
 import tkinter as tk
+from turtle import width
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
@@ -11,28 +12,63 @@ root = ttk.Window(themename="darkly")
 # frmFln = ttk.Frame(root, bootstyle="light", width=150, height=35)
 
 btnBrowse = ttk.Button(root, text="Browse", bootstyle="default", width=12)
-btnBrowse.grid(row=0, column=0, padx=50, pady=25)
+btnBrowse.grid(row=0, column=0, padx=(50,20), pady=(50,25))
 
-lblFln = ttk.Label(root, text="Folder name", bootstyle="default", width=12, justify='left')
-lblFln.grid(row=0, column=1, columnspan=2, padx=20, pady=25)
+folderName = tk.StringVar(value='Folder name')
 
-lblNewFln = ttk.Label(root, text="Input new file name : ", bootstyle="default", justify='left')
-lblNewFln.grid(row=1, column=0, padx=(50,0), pady=25)
+lblFolderName = ttk.Label(root, textvariable=folderName, bootstyle="default", justify='left')
+lblFolderName.grid(row=0, column=1, columnspan=2, padx=(20,50), pady=(50,25))
 
-inputNewFln = ttk.Entry(root, bootstyle="default", width=20)
-inputNewFln.grid(row=1, column=1, padx=(0,50), pady=25)
+
+frmNewFln = ttk.Frame(root, bootstyle="default")
+frmNewFln.grid(row=1, column=0, padx=(50,30), pady=25)
+
+fln = tk.StringVar(value='File')
+
+lblNewFln = ttk.Label(frmNewFln, text="Input new file name : ", bootstyle="default", justify='left')
+lblNewFln.grid(row=0, column=0, padx=(20,5), pady=25)
+
+inputNewFln = ttk.Entry(frmNewFln, bootstyle="default", width=18, textvariable=fln)
+inputNewFln.grid(row=0, column=1, padx=(5,20), pady=25)
 
 btnRename = ttk.Button(root, text="Rename", bootstyle="success", width=12)
-btnRename.grid(row=1, column=2, padx=20, pady=25)
+btnRename.grid(row=1, column=1, padx=(30,50), pady=25)
 
-checkbuttonOrder = ttk.Checkbutton(bootstyle="success")
-checkbuttonOrder.grid(row=2, column=0, padx=(50,0), pady=25)
 
-lblOrder = ttk.Label(root, text="Add prefix order number", bootstyle="default", justify='left')
-lblOrder.grid(row=2, column=1, padx=(0,50), pady=25)
+btnMenuOrder = ttk.Menubutton(root, bootstyle="secondary", text='Position order', width=12)
+btnMenuOrder.grid(row=2, column=0, padx=(50,30), pady=(0,25))
 
-lblReport = ttk.Label(root, text="1 file was successfully renamed", bootstyle="default", justify='left')
-lblReport.grid(row=3, column=0, columnspan=3, padx=50, pady=30)
+menuOrder = tk.Menu(btnMenuOrder, tearoff=0)
+posOrders = ('No order', 'Prefix', 'Suffix')
+choiceOrder = tk.StringVar()
+for posOrder in posOrders:
+    menuOrder.add_radiobutton(
+        label=posOrder,
+        value=posOrder,
+        variable=choiceOrder)
+btnMenuOrder["menu"] = menuOrder
+
+frmEraseSymbol = ttk.Frame(root, bootstyle="default")
+frmEraseSymbol.grid(row=2, column=1, padx=(30,50), pady=(0,25))
+
+checkbuttonEraseSymbol = ttk.Checkbutton(frmEraseSymbol, bootstyle="secondary")
+checkbuttonEraseSymbol.grid(row=0, column=0, padx=(20,5), pady=25)
+
+lblEraseSymbol = ttk.Label(frmEraseSymbol, text="Erase symbol", bootstyle="default", justify='left')
+lblEraseSymbol.grid(row=0, column=1, padx=(5,20), pady=25)
+
+
+sumFiles = tk.StringVar(value=0)
+lblReport = ttk.Label(root, text=f"{sumFiles.get()} file was successfully renamed", bootstyle="default", justify='left')
+lblReport.grid(row=3, column=0, columnspan=4, padx=50, pady=(30,50))
+
+if int(sumFiles.get()) < 0:
+    sumFiles.set(0)
+
+if int(sumFiles.get()) == 1 or int(sumFiles.get()) == 0:
+    lblReport.configure(text=f"{sumFiles.get()} file was successfully renamed")
+elif int(sumFiles.get()) > 1:
+    lblReport.configure(text=f"{sumFiles.get()} files was successfully renamed")
 
 
 
